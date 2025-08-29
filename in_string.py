ab2fa08bbc4af61ff782198520e2a50447b0e6c5
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-08-29 12:32:27 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,7 +1,12 @@
 def check_vowels():
     nombre = input("> ").lower()
-    print(f"Contiene a: {"a" in nombre}")
-    print(f"Contiene e: {"e" in nombre}")
-    print(f"Contiene i: {"i" in nombre}")
-    print(f"Contiene o: {"o" in nombre}")
-    print(f"Contiene u: {"u" in nombre}")
+    vocalA = f"Contiene a: {"a" in nombre}"
+    vocalE = f"Contiene e: {"e" in nombre}"
+    vocalI = f"Contiene i: {"i" in nombre}"
+    vocalO = f"Contiene o: {"o" in nombre}"
+    vocalU = f"Contiene u: {"u" in nombre}"
+    print(vocalA)
+    print(vocalE)
+    print(vocalI)
+    print(vocalO)
+    print(vocalU)
